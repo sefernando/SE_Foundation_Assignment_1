e58a18580aad916d6a7a8eb98e5beac7cf29486c
wmsefernando <s.eranga@gmail.com>
2022-05-20 16:15:41 +0800

    signup done

--- Server/service/authService.js
+++ Server/service/authService.js
@@ -1,16 +1,65 @@
 const { User } = require("../models");
+const bcryptjs = require("bcryptjs");
+const { Op } = require("sequelize");
+const jwt = require("jsonwebtoken");
 
-//signup ---------------------------------------
+//signup ---------------------------------------------------------------------
 async function signUp(req, res) {
+  const saltRounds = 10;
+  const pwd = req.body.password;
+  let user = null;
+
   //check if the user already exist
-  const existingUser = await User.findOne({ where: { email: req.body.email } });
+  // const userByEmail = await User.findOne({ where: { email: req.body.email } });
+  // const userByUserName = await User.findOne({
+  //   where: { userName: req.body.userName },
+  // });
+
+  const existingUser = await User.findOne({
+    where: {
+      [Op.or]: [{ email: req.body.email }, { userName: req.body.userName }],
+    },
+  });
+
+  // if (userByEmail !== null || userByUserName !== null)
   if (existingUser !== null) {
     return res
       .status(400)
       .json({ errors: [{ msg: "user is already registered" }] });
   }
 
-  res.send("sign up ok");
+  //hashing password
+  const hashPassword = await bcryptjs.hash(pwd, saltRounds);
+
+  //saving user to DB
+  if (hashPassword) {
+    user = await User.create({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashPassword,
+    });
+  } else {
+    return res
+      .status(500)
+      .json({ errors: [{ msg: "password hashing error" }] });
+  }
+
+  //creating jwt token
+  const payLoad = { userName: user.userName, id: user.id };
+  const privateKey = process.env.TOKEN_SECRET;
+  const options = { expiresIn: process.env.TOKEN_EXPIRE };
+
+  jwt.sign(payLoad, privateKey, options, (err, token) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ errors: [{ msg: "error occured when creating jwt token" }] });
+    } else {
+      return res.json({ token });
+    }
+  });
 }
+
+//signin ---------------------------------------------------------------------
 
 module.exports = { signUp };
