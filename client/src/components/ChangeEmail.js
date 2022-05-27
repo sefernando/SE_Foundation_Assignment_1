0288268056c6e1e22a1fe47a6b0d21650587ecad
wmsefernando <s.eranga@gmail.com>
2022-05-27 14:46:57 +0800

    change email done

---
+++ client/src/components/ChangeEmail.js
@@ -0,0 +1,78 @@
+import { useRef, useState, useEffect, useContext } from "react";
+import axios from "../api/axios";
+
+import AuthContext from "../context/AuthProvider";
+
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]{2,}\.[^\s@]{2,}$/;
+const CHANGE_EMAIL_URL = "auth/changeEmail";
+
+const ChangeEmail = () => {
+  const emailRef = useRef();
+  const { auth } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(email));
+  }, [email]);
+
+  //handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const userName = auth.userName;
+
+    try {
+      const response = await axios.put(
+        CHANGE_EMAIL_URL,
+        JSON.stringify({ userName, email }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-token": auth.token,
+          },
+          withCredentials: true,
+        }
+      );
+      if (response) console.log("email successfully changed");
+    } catch (error) {
+      if (!error.response) {
+        setErrMsg("No server response");
+      } else {
+        setErrMsg("Email updating Failed");
+      }
+    }
+  };
+
+  return (
+    <section>
+      <p className="errMsg">{errMsg}</p>
+      <h2>Change Email</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="chnageEmai">Email</label>
+          <input
+            type="email"
+            name="changeEmail"
+            id="changeEmail"
+            ref={emailRef}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {email && !validEmail && (
+            <small style={{ color: "red" }}>Enter a valid email</small>
+          )}
+        </div>
+        <button>Submit</button>
+      </form>
+    </section>
+  );
+};
+
+export default ChangeEmail;
--- client/src/pages/UserManagement.js
+++ client/src/pages/UserManagement.js
@@ -1,10 +1,12 @@
 import Register from "../components/Register";
 import ChangePassword from "../components/ChangePassword";
+import ChangeEmail from "../components/ChangeEmail";
 
 const UserManagement = () => {
   return (
     <>
       <ChangePassword />
+      <ChangeEmail />
       <Register />
     </>
   );
